2f4406e7256bd6bd9a72b496d43d61c3ab09d1a4
Szilvia-Mezo <Szilvia.Mezo@ibm.com>
2023-01-18 15:49:45 +0100

    env test 2

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -17,5 +17,5 @@ if __name__ == '__main__':
     run_command('git init')
     run_command('npm install')
     run_command('npm test')
-    run_command('ls -la')
-    run_command('scripts/start.sh ' + kata_name)
+    run_command('ls -la scripts/')
+    run_command('./scripts/start.sh ' + kata_name)
