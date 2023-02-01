438431f040c7091bfb1fed11c376e67ad92cddf0
Szilvia-Mezo <Szilvia.Mezo@ibm.com>
2023-02-01 15:21:15 +0100

    github env fix 7

--- {{cookiecutter.directory_name}}/scripts/postworks.sh
+++ {{cookiecutter.directory_name}}/scripts/postworks.sh
@@ -40,10 +40,10 @@ function gh_authentication_check () {
 function create_the_repo () {
   echo " "
   echo "⚙️  creating the repository"
-  gh repo create $GIT_ORG/$NEW_REPO_NAME --public --confirm
+  gh repo create $GIT_ORG/$NEW_REPO_NAME --public --remote=origin
   git init
   git branch -M main
-  git remote add origin https://github.com/$GIT_ORG/$NEW_REPO_NAME
+#  git remote add origin https://github.com/$GIT_ORG/$NEW_REPO_NAME
   check_for_errors $? "☑️  Repository cloned correctly"  "💥 GitHub client 🙀 terminated with an unexpected exit code..." 
 }
 
